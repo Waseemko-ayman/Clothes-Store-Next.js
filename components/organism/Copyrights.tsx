0b26d4b1ad7f646ac3a25cebccbdb9fd5c68b751
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-06-21 10:40:45 +0300

    Used animation from framer-motion with Copyrights Section

--- components/organism/Copyrights.tsx
+++ components/organism/Copyrights.tsx
@@ -1,15 +1,23 @@
+'use client';
 import React from 'react';
 import Container from '../atoms/Container';
+import { motion } from 'framer-motion';
 
 const Copyrights = () => {
   const dateNow = new Date();
   return (
     <div className="text-center">
       <Container>
-        <p className="text-base text-[#465b52] my-5">
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.2 }}
+          transition={{ duration: 0.6 }}
+          className="text-base text-[#465b52] my-5"
+        >
           &copy; {dateNow.getFullYear()}, Programmed by Waseem Abd Elhadi
           Developer
-        </p>
+        </motion.p>
       </Container>
     </div>
   );
