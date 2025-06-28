74ba6374dcd20b83db5b7f92b20dc838910d6b9b
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-06-28 19:56:25 +0300

    Build Contact Page

---
+++ components/pages/contact/Sections/RepairServices.tsx
@@ -0,0 +1,14 @@
+import RepairServicesComp from '@/components/molecules/RepairServicesSection';
+import React from 'react';
+
+const RepairServices = () => {
+  return (
+    <RepairServicesComp
+      subTitle="#let's_talk"
+      description="LEAVE A MESSAGE. We love to hear from you!"
+      bgImage="/assets/about/banner.png"
+    />
+  );
+};
+
+export default RepairServices;
