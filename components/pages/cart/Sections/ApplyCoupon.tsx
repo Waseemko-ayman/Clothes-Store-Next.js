e68a3ede755e41b634236eb5055e7bc818368837
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-04 22:29:51 +0300

    Make Button in Apply Coupon Section full Width in max width [991px]

--- components/pages/cart/Sections/ApplyCoupon.tsx
+++ components/pages/cart/Sections/ApplyCoupon.tsx
@@ -34,13 +34,16 @@ const ApplyCoupon = () => {
       <Container otherClassName="flex items-start justify-between gap-5 max-[991px]:flex-wrap max-[991px]:justify-center max-[991px]:gap-[30px]">
         <div>
           <h3 className={TitleStyle}>Apply Coupon</h3>
-          <form className="flex items-center gap-2.5 max-w-full w-full max-[991px]:flex-col">
+          <form className="flex items-center gap-2.5 max-w-full w-full max-[991px]:flex-col max-[991px]:text-center">
             <input
               type="text"
               placeholder="Enter Your Coupon"
               className={`${formElStyle} p-2.5 max-w-full w-[400px] border border-[var(--seven-color)] max-md:w-[300px] placeholder:transition-all placeholder:duration-300 focus:placeholder:opacity-0`}
             />
-            <Button variant="primary" otherClassName="py-[5px] px-[15px]">
+            <Button
+              variant="primary"
+              otherClassName="py-[5px] px-[15px] max-[991px]:w-full"
+            >
               Apply
             </Button>
           </form>
