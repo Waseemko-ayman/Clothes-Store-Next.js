3954580b305deadcf751700c35fb86148bb27054
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-12-06 10:47:25 +0200

    fix(NavLinks): highlight active link based on pathname, including Home

--- components/molecules/NavLinks.tsx
+++ components/molecules/NavLinks.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import React from 'react';
 import { FaCartShopping } from 'react-icons/fa6';
 import { motion } from 'framer-motion';
+import { useCartContext } from '@/context/CartContext';
+import { usePathname } from 'next/navigation';
 
 const NavLinks = ({
   isMobile,
@@ -12,6 +14,9 @@ const NavLinks = ({
   isMobile?: boolean;
   onLinkClick?: () => void;
 }) => {
+  const pathname = usePathname();
+  const { cartItems } = useCartContext();
+
   const StyledLinks = `relative py-1 text-base font-semibold cursor-pointer text-[var(${
     isMobile ? '--white-color' : '--fifth-color'
   })] hover:text-[var(--forth-color)] transition duration-200`;
@@ -37,7 +42,13 @@ const NavLinks = ({
               item.name !== 'Cart' &&
               !isMobile &&
               'after:absolute after:left-0 after:bottom-0 after:bg-[var(--forth-color)] after:w-0 after:h-0.5 hover:after:w-1/2 after:transition-all after:duration-300'
-            } ${isMobile ? 'text-lg mb-4' : ''}`}
+            } ${isMobile ? 'text-lg mb-4' : ''} ${
+              pathname === '/' && item.name.toLowerCase() === 'home'
+                ? 'after:w-1/2 text-[var(--forth-color)]'
+                : pathname.slice(1) === item.name.toLowerCase()
+                ? 'after:w-1/2 text-[var(--forth-color)]'
+                : ''
+            }`}
             onClick={onLinkClick}
           >
             <Link href={item.link}>
@@ -45,7 +56,7 @@ const NavLinks = ({
                 <div className="flex items-center gap-1">
                   <FaCartShopping className={`${StyledLinks}`} size="25px" />
                   <span className="text-[var(--white-color)] bg-[var(--forth-color)] w-fit py-0.5 px-2 rounded-sm text-sm text-center font-bold">
-                    0
+                    {cartItems?.length}
                   </span>
                 </div>
               ) : (
