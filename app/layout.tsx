be0e4edd21122e1135e2c89706d183db1e44a48f
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-12-08 15:33:44 +0200

    Integrate Cart and Products contexts across the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import MainLayout from '@/components/organism/MainLayout';
+import { CartProvider } from '@/context/CartContext';
+import { ProductsProvider } from '@/context/ProductsContext';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,12 +28,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.png" sizes="any" />
+        <link rel="icon" href="/assets/landing/logo.png" sizes="any" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MainLayout>{children}</MainLayout>
+        <ProductsProvider>
+          <CartProvider>
+            <MainLayout>{children}</MainLayout>
+          </CartProvider>
+        </ProductsProvider>
       </body>
     </html>
   );
--- components/pages/home/Sections/NewArrivals.tsx
+++ components/pages/home/Sections/NewArrivals.tsx
@@ -1,18 +1,23 @@
 'use client';
 import Container from '@/components/atoms/Container';
 import Layer from '@/components/atoms/Layer';
+import Loading from '@/components/atoms/Loading';
 import MainTitle from '@/components/atoms/MainTitle';
 import ProdcutsContainer from '@/components/atoms/ProdcutsContainer';
 import AnimatedWrapper from '@/components/molecules/FramerMotion/AnimatedWrapper';
 import ProductCard from '@/components/molecules/ProductCard';
-import { CLOTHES } from '@/mock';
+import { useProductsContext } from '@/context/ProductsContext';
 import { PATHS } from '@/mock/paths';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const NewArrivals = () => {
   const router = useRouter();
-  const newArrivals = CLOTHES.filter((p) => p.section === 'newArrivals');
+
+  // API Context
+  const { clothes, isLoading } = useProductsContext();
+
+  const newArrivals = clothes.filter((p) => p.section === 'newArrivals');
   return (
     <Layer>
       <Container>
@@ -20,23 +25,27 @@ const NewArrivals = () => {
           title="New Arrivals"
           description="Summer Collection New Modern Design"
         />
-        <ProdcutsContainer>
-          {newArrivals.map(
-            ({ id, src, imgText, tradeMark, productTitle, price }, index) => (
-              <AnimatedWrapper key={id} custom={index}>
-                <ProductCard
-                  key={id}
-                  imgSrc={src}
-                  imgText={imgText}
-                  tradeMark={tradeMark}
-                  productTitle={productTitle}
-                  price={price}
-                  handleClick={() => router.push(PATHS.SHOP.ITEM(id))}
-                />
-              </AnimatedWrapper>
-            )
-          )}
-        </ProdcutsContainer>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <ProdcutsContainer>
+            {newArrivals.map(
+              ({ id, src, imgText, tradeMark, productTitle, price }, index) => (
+                <AnimatedWrapper key={id} custom={index}>
+                  <ProductCard
+                    key={id}
+                    imgSrc={src}
+                    imgText={imgText}
+                    tradeMark={tradeMark}
+                    productTitle={productTitle}
+                    price={price}
+                    handleClick={() => router.push(PATHS.SHOP.ITEM(id))}
+                  />
+                </AnimatedWrapper>
+              )
+            )}
+          </ProdcutsContainer>
+        )}
       </Container>
     </Layer>
   );
