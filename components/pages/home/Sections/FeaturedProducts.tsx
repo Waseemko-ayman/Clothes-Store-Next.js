7d1734498c05da45b293cca1a28ac363277c0a01
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-12-08 15:55:01 +0200

    Use useProductsContext in FeaturedProducts section in home page

--- components/pages/home/Sections/FeaturedProducts.tsx
+++ components/pages/home/Sections/FeaturedProducts.tsx
@@ -2,18 +2,23 @@
 
 import Container from '@/components/atoms/Container';
 import Layer from '@/components/atoms/Layer';
+import Loading from '@/components/atoms/Loading';
 import MainTitle from '@/components/atoms/MainTitle';
 import ProdcutsContainer from '@/components/atoms/ProdcutsContainer';
 import AnimatedWrapper from '@/components/molecules/FramerMotion/AnimatedWrapper';
 import ProductCard from '@/components/molecules/ProductCard';
-import { CLOTHES } from '@/mock';
+import { useProductsContext } from '@/context/ProductsContext';
 import { PATHS } from '@/mock/paths';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const FeaturedProducts = () => {
   const router = useRouter();
-  const featuredProducts = CLOTHES.filter((p) => p.section === 'featured');
+
+  // API Context
+  const { clothes, isLoading } = useProductsContext();
+
+  const featuredProducts = clothes.filter((p) => p.section === 'featured');
   return (
     <Layer>
       <Container>
@@ -21,23 +26,27 @@ const FeaturedProducts = () => {
           title="Featured Product"
           description="Summer Collection New Modern Design"
         />
-        <ProdcutsContainer>
-          {featuredProducts.map(
-            ({ id, src, imgText, tradeMark, productTitle, price }, index) => (
-              <AnimatedWrapper key={id} custom={index}>
-                <ProductCard
-                  key={id}
-                  imgSrc={src}
-                  imgText={imgText}
-                  tradeMark={tradeMark}
-                  productTitle={productTitle}
-                  price={price}
-                  handleClick={() => router.push(PATHS.SHOP.ITEM(id))}
-                />
-              </AnimatedWrapper>
-            )
-          )}
-        </ProdcutsContainer>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <ProdcutsContainer>
+            {featuredProducts.map(
+              ({ id, src, imgText, tradeMark, productTitle, price }, index) => (
+                <AnimatedWrapper key={id} custom={index}>
+                  <ProductCard
+                    key={id}
+                    imgSrc={src}
+                    imgText={imgText}
+                    tradeMark={tradeMark}
+                    productTitle={productTitle}
+                    price={price}
+                    handleClick={() => router.push(PATHS.SHOP.ITEM(id))}
+                  />
+                </AnimatedWrapper>
+              )
+            )}
+          </ProdcutsContainer>
+        )}
       </Container>
     </Layer>
   );
