33a90fdbc3ea1e8a157d18298ef7c0205ebb93fb
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-12-08 15:36:05 +0200

    Create types and interaces files

---
+++ utils/types.ts
@@ -0,0 +1,15 @@
+export type ButtonMainVarinats =
+  | 'primary'
+  | 'secondary'
+  | 'outline'
+  | 'cover'
+  | 'circle';
+export type ButtonVarinats =
+  | ButtonMainVarinats
+  | 'third'
+  | 'forth'
+  | 'fifth'
+  | 'ghost'
+  | 'circle';
+export type ButtonTypes = 'submit' | 'button' | 'reset';
+export type ButtonIconPosition = 'left' | 'right';
