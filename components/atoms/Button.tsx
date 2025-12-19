26403a1638465ad1324553789183fea743f1fbb5
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-12-19 23:09:31 +0200

    Some Edits in Button Component

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -18,6 +18,7 @@ const Button = forwardRef<
       iconPosition = 'left',
       disabled = false,
       href,
+      iconClassName,
       // bgColor,
       // hoverBgColor,
       ...props
@@ -25,26 +26,29 @@ const Button = forwardRef<
     ref
   ) => {
     const classes = `${
-      variant !== 'cover' ? 'py-3 px-8 hover:tracking-widest' : 'py-4 px-20'
+      // variant !== 'cover' ? 'py-3 px-8 hover:tracking-widest' : 'py-4 px-20'
+      variant !== 'cover' ? 'py-3 px-8' : 'py-4 px-20'
     } cursor-pointer text-base outline-none font-semibold transition-all duration-200 ${borderRadius} ${
       variant === 'primary'
         ? 'bg-[var(--forth-color)] text-[var(--white-color)] hover:bg-[#054b46]'
         : variant === 'secondary'
         ? 'bg-[var(--white-color)] text-black hover:bg-[var(--forth-color)] hover:text-[var(--white-color)]'
         : variant === 'outline'
-        ? 'bg-transparent border border-[var(--white-color)] text-[var(--white-color)] hover:bg-[var(--forth-color)] hover:text-[var(--white-color)] hover:border-[var(--forth-color)]'
+        ? 'bg-transparent border border-[var(--forth-color)] text-[var(--white-color)] hover:bg-[var(--forth-color)] hover:text-[var(--white-color)] hover:border-[var(--forth-color)]'
         : variant === 'cover'
         ? 'bg-center bg-transparent bg-no-repeat text-[var(--forth-color)]'
         : variant === 'circle'
         ? 'w-10 h-10 !p-0 bg-[#e8f6ea] text-[var(--forth-color)] hover:bg-[var(--forth-color)] hover:text-[var(--white-color)] hover:border-[var(--white-color)] hover:rotate-[360deg] border border-[#cce7d0] !rounded-[50%] transition-all duration-200'
+        : variant === 'ghost'
+        ? 'text-[var(--forth-color)] hover:bg-gray-200'
         : ''
-    } ${otherClassName}`;
+    } ${Icon ? 'flex items-center gap-2' : ''} ${otherClassName}`;
 
     const content = (
       <>
-        {iconPosition === 'left' && Icon && <Icon />}
+        {iconPosition === 'left' && Icon && <Icon className={iconClassName} />}
         {children}
-        {iconPosition === 'right' && Icon && <Icon />}
+        {iconPosition === 'right' && Icon && <Icon className={iconClassName} />}
       </>
     );
 
