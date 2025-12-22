644d376f3157fa544ba522cce27a3505ec2c4635
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-12-22 09:08:58 +0200

    Used AuthTemplate in Profile, and PasswordSettings Components

--- components/pages/my-account/Sections/Profile.tsx
+++ components/pages/my-account/Sections/Profile.tsx
@@ -1,34 +1,47 @@
-import Button from '@/components/atoms/Button';
+'use client';
 import Input from '@/components/atoms/Input';
 import AccountSectionHeader from '@/components/molecules/AccountSectionHeader';
+import AuthTemplate from '@/components/Template/AuthTemplate';
 import { profileSecInputs } from '@/mock';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Profile = () => {
+  const [loading] = useState(false);
+  const [error] = useState('');
   return (
     <div>
       <AccountSectionHeader
         title="Profile Information"
         description="Update your personal details"
       />
-      <div className="pt-6 space-y-6">
-        <div className="grid md:grid-cols-2 gap-4">
-          {profileSecInputs.map((input) => (
-            <div key={input.id} className="space-y-2">
-              <Input
-                id={input.name}
-                type={input.type}
-                label={input.label}
-                inputName={input.name}
-                placeholder={input.placeholder}
-                otherClassName="w-full"
-                // defaultValue="John"
-              />
+      <AuthTemplate
+        otherClassName="mt-6 !max-w-full rounded-xl"
+        error={error}
+        submitBtnText="Save Changes"
+        loadingText="save changes..."
+        loading={loading}
+        formChildren={
+          <>
+            <div className="grid md:grid-cols-2 gap-4">
+              {profileSecInputs.map((input) => (
+                <div key={input.id} className="space-y-2">
+                  <Input
+                    id={input.name}
+                    type={input.type}
+                    label={input.label}
+                    inputName={input.name}
+                    placeholder={input.placeholder}
+                    otherClassName="w-full"
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </>
+        }
+      />
+      {/* <div className="pt-6 space-y-6">
         <Button>Save Changes</Button>
-      </div>
+      </div> */}
     </div>
   );
 };
