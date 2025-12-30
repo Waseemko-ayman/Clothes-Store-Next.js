829f4144bc07b852a55dbe8f965d3c9dcc5254bc
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-12-30 18:09:37 +0200

    Product Card Update and Development

--- components/molecules/ProductCard.tsx
+++ components/molecules/ProductCard.tsx
@@ -1,61 +1,109 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import Image from 'next/image';
 import React from 'react';
 import Button from '../atoms/Button';
 import { FaStar } from 'react-icons/fa';
-import { FaCartShopping } from 'react-icons/fa6';
+import { FaCartShopping, FaRegStar, FaStarHalfStroke } from 'react-icons/fa6';
 import { useToast } from '@/lib/toast';
 import { useCartContext } from '@/context/CartContext';
-
-interface ProductCardProps {
-  imgSrc: string;
-  imgText: string;
-  tradeMark: string;
-  productTitle: string;
-  price: number;
-  productData: any;
-  handleClick?: () => void;
-}
+import { ProductCardProps } from '@/interfaces';
 
 const ProductCard = ({
-  imgSrc,
-  imgText,
-  tradeMark,
-  productTitle,
-  price,
   productData,
   handleClick,
+  otherClassName,
 }: ProductCardProps) => {
+  const { image, title, trade_mark, price, old_price, discount, ratings } =
+    productData;
+
   // Contexts
   const { addToCart } = useCartContext();
   const { showToast } = useToast();
 
+  // حساب متوسط النجوم من الـ stars في كل rating object
+  const averageRating =
+    ratings && ratings?.length > 0
+      ? ratings.reduce((sum: number, r: number) => sum + r, 0) / ratings.length
+      : 0; // بدل null حطينا 0
+
+  const renderStars = (rating: number) => {
+    const stars = [];
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 >= 0.5;
+
+    // النجوم الممتلئة
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(
+        <FaStar key={`full-${i}`} className="text-lg text-yellow-500" />
+      );
+    }
+
+    // النصف نجمة إن وجد
+    if (hasHalfStar) {
+      stars.push(
+        <FaStarHalfStroke key="half" className="text-lg text-yellow-500" />
+      );
+    }
+
+    // النجوم الفارغة (نكمل إلى 5 نجوم)
+    const emptyStars = 5 - (fullStars + (hasHalfStar ? 1 : 0));
+    for (let i = 0; i < emptyStars; i++) {
+      stars.push(
+        <FaRegStar key={`empty-${i}`} className="text-lg text-gray-400" />
+      );
+    }
+
+    return stars;
+  };
+
   return (
     <div
-      className="relative max-w-full p-3.5 border border-[#cce7d0] rounded-[20px] shadow-[20px_20px_34px_rgb(0, 0, 0, 0.03)] hover:shadow-[10px_10px_54px_#ddd] hover:scale-[1.02] cursor-pointer transition-all duration-300"
+      className={`relative max-w-full p-3.5 border border-[#cce7d0] rounded-[20px] shadow-[20px_20px_34px_rgb(0, 0, 0, 0.03)] hover:shadow-[10px_10px_54px_#ddd] hover:scale-[1.02] cursor-pointer transition-all duration-300 ${otherClassName}`}
       onClick={handleClick}
     >
-      <Image
-        src={`/assets/products/${imgSrc}.jpg`}
-        alt={imgText}
-        title={imgText}
-        width={500}
-        height={500}
-        className="max-w-full rounded-[20px] mb-2.5"
-      />
+      <div className="relative">
+        <Image
+          src={`/assets/products/${image}.jpg`}
+          alt={title}
+          title={title}
+          width={500}
+          height={500}
+          className="max-w-full rounded-[20px] mb-2.5"
+        />
+        {discount && (
+          <span className="absolute top-2 right-2 bg-red-600 text-white text-[10px] font-semibold px-2 py-1 rounded-lg shadow-md z-10">
+            rival %{discount}
+          </span>
+        )}
+      </div>
       <div>
-        <span className="text-base text-[var(--first-color)]">{tradeMark}</span>
+        <span className="text-base text-[var(--first-color)]">
+          {trade_mark && trade_mark}
+        </span>
         <h4 className="text-[var(--forth-color)] text-base mt-2 font-bold">
-          {productTitle}
+          {title}
         </h4>
+
         <div className="flex items-center gap-1">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <FaStar key={index} size="18px" className="text-amber-300 my-2" />
-          ))}
+          {renderStars(averageRating)}
+          <span className="ml-1 font-semibold">{averageRating.toFixed(1)}</span>
         </div>
-        <h5 className="text-[var(--forth-color)] text-lg font-bold">
-          ${price}
-        </h5>
+
+        <div className="flex items-center gap-2">
+          {price && (
+            <h5 className="text-[var(--forth-color)] text-lg font-bold">
+              ${price}
+            </h5>
+          )}
+          {old_price && (
+            <h4
+              className={`font-semibold ${
+                price ? 'line-through text-gray-400 text-sm' : 'text-[15px]'
+              }`}
+            >
+              ${old_price}
+            </h4>
+          )}
+        </div>
       </div>
       <Button
         variant="circle"
@@ -63,7 +111,7 @@ const ProductCard = ({
         handleClick={(e) => {
           e.stopPropagation();
           addToCart?.(productData);
-          showToast(`Add ${productTitle} to cart`);
+          showToast(`Add ${title} to cart`);
         }}
       >
         <FaCartShopping size="20px" />
