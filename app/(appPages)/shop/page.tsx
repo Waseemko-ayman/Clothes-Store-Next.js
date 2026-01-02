5edc447804a3bd8d13423082766b45f8a5e0de15
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-01-02 19:00:52 +0200

    Retrieve products from the server to the Shop page

--- app/(appPages)/shop/page.tsx
+++ app/(appPages)/shop/page.tsx
@@ -1,4 +1,5 @@
 import ShopPage from '@/components/pages/shop';
+import { API_URL } from '@/config/api';
 import { Metadata } from 'next';
 import React from 'react';
 
@@ -7,6 +8,15 @@ export const metadata: Metadata = {
   description: 'Browse and purchase clothes from Clothes Shopping',
 };
 
-const Shop = () => <ShopPage />;
+async function getProducts() {
+  const res = await fetch(`${API_URL}/clothes`, { cache: 'no-store' });
+  return res.json();
+}
+
+const Shop = async () => {
+  const products = await getProducts();
+
+  return <ShopPage products={products} />;
+};
 
 export default Shop;
