41c7e3b2a45e0e45c2068150efbf7e66fb99da85
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-01-17 21:25:43 +0200

    Logic separates the useSupabaseClient Hook, located in the useEffect, into a separate fetchData function to enable its real-time updating.

--- Hooks/useSupabaseClient.tsx
+++ Hooks/useSupabaseClient.tsx
@@ -12,54 +12,56 @@ export default function useSupabaseClient(
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
-  useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      try {
-        let supabaseRef = supabase.from(tableName).select();
-        if (filters) {
-          for (const [column, value] of Object.entries(filters)) {
-            if (value !== undefined && value !== null) {
-              if (column === 'title') {
-                // ilike: It performs case-insensitive text search.
-                // %: It will return all products where the text is part of the title; without it, it will only return the title that matches the text completely.
-                supabaseRef = supabaseRef.ilike(column, `%${value}%`);
-              } else if (column === 'discount' && value === true) {
-                supabaseRef = supabaseRef.gt('discount', 0); // .gt: Greater than ex: 0
-              } else if (column === 'rating_avg' && value === true) {
-                // Uses `order()` for sorting.
-                supabaseRef = supabaseRef.order('rating_avg', {
-                  ascending: false, // From largest to smallest
-                });
-              } else if (column === 'price') {
-                supabaseRef = supabaseRef.order('price', {
-                  ascending: value,
-                });
-              } else {
-                supabaseRef = supabaseRef.eq(column, value);
-              }
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      let supabaseRef = supabase.from(tableName).select('*');
+      if (filters) {
+        for (const [column, value] of Object.entries(filters)) {
+          if (value !== undefined && value !== null) {
+            if (column === 'title') {
+              // ilike: It performs case-insensitive text search.
+              // %: It will return all products where the text is part of the title; without it, it will only return the title that matches the text completely.
+              supabaseRef = supabaseRef.ilike(column, `%${value}%`);
+            } else if (column === 'discount' && value === true) {
+              supabaseRef = supabaseRef.gt('discount', 0); // .gt: Greater than ex: 0
+            } else if (column === 'rating_avg' && value === true) {
+              // Uses `order()` for sorting.
+              supabaseRef = supabaseRef.order('rating_avg', {
+                ascending: false, // From largest to smallest
+              });
+            } else if (column === 'price') {
+              supabaseRef = supabaseRef.order('price', {
+                ascending: value,
+              });
+            } else {
+              supabaseRef = supabaseRef.eq(column, value);
             }
           }
         }
-
-        // Price filtering
-        if (priceRange) {
-          const [minPrice, maxPrice] = priceRange;
-          if (minPrice !== undefined)
-            supabaseRef = supabaseRef.gte('price', minPrice); // أكبر أو يساوي
-
-          if (maxPrice !== undefined)
-            supabaseRef = supabaseRef.lte('price', maxPrice); // أقل أو يساوي
-        }
-
-        const { data } = await supabaseRef;
-        setData(data);
-        setIsLoading(false);
-      } catch (error: unknown) {
-        setError(error instanceof Error ? error.message : String(error));
-        setIsLoading(false);
       }
-    })();
+
+      // Price filtering
+      if (priceRange) {
+        const [minPrice, maxPrice] = priceRange;
+        if (minPrice !== undefined)
+          supabaseRef = supabaseRef.gte('price', minPrice); // أكبر أو يساوي
+
+        if (maxPrice !== undefined)
+          supabaseRef = supabaseRef.lte('price', maxPrice); // أقل أو يساوي
+      }
+
+      const { data } = await supabaseRef;
+      setData(data);
+      setIsLoading(false);
+    } catch (error: unknown) {
+      setError(error instanceof Error ? error.message : String(error));
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
 
     // Use JSON.stringify to compare object values instead of references.
     // Prevents unnecessary reruns when the object is recreated with same data.
@@ -87,5 +89,5 @@ export default function useSupabaseClient(
     }
   };
 
-  return { data, error, isLoading, insertData };
+  return { data, error, isLoading, insertData, refetch: fetchData };
 }
