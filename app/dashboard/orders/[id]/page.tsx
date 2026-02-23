986ba504426d0fd0e750a3394866a44866559a0f
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-02-23 21:51:31 +0200

    Delete orders, settings, and ratings pages from dashboard

--- app/dashboard/orders/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import OrderItemsPage from '@/components/pages/dashboard/orders/OrderItems';
-import React from 'react';
-
-const OrderItems = () => <OrderItemsPage />;
-
-export default OrderItems;
--- app/dashboard/orders/page.tsx
+++
@@ -1,6 +0,0 @@
-import OrdersPage from '@/components/pages/dashboard/orders';
-import React from 'react';
-
-const Orders = () => <OrdersPage />;
-
-export default Orders;
