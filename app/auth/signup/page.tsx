1651889b6e13f8f4e37095691067098fc8447f61
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-02-24 22:14:38 +0200

    Change Metadate Title to Auth Pages

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 import React from 'react';
 
 export const metadata: Metadata = {
-  title: 'Signup - MyApp',
+  title: 'Signup - Wénor Shop',
 };
 
 const Signup = () => <SignupPage />;
